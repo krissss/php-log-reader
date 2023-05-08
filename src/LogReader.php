d4990de2a49197ffd67fa7c06c4c1745a053858e
kriss <462679766@qq.com>
2023-05-08 18:42:27 +0800

    change cdn

--- src/LogReader.php
+++ src/LogReader.php
@@ -47,7 +47,7 @@ class LogReader
      * bootstrap.css url
      * @var string
      */
-    private $bootstrapV3CssUrl = 'https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css';
+    private $bootstrapV3CssUrl = 'https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css';
 
     public function __construct(string $logPath, $config = [])
     {
